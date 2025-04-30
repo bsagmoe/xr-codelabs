06a8afc0e87dcfb138bc527dc6c185eebb490cd4
Ben Sagmoe <bsagmoe@google.com>
2025-04-30 17:02:12 -0400

    Revisions

--- xr-fundamentals/part2/app/src/main/java/com/example/android/xrfundamentals/XRFundamentalsApp.kt
+++ xr-fundamentals/part2/app/src/main/java/com/example/android/xrfundamentals/XRFundamentalsApp.kt
@@ -16,6 +16,7 @@
 
 package com.example.android.xrfundamentals
 
+import android.util.Log
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
